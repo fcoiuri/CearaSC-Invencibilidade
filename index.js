516b7d572766fdaee34fc30553373fa00e76f849
FranciscoIuri <fcoiuri@gmail.com>
2020-05-12 19:30:06 -0300

    make it better

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 app.set("view engine", "ejs")
 app.set("views", "./views")
 
-app.use(express.static('./views'))
+app.use(express.static('views'))
 app.listen(process.env.PORT || 3000);
 
 app.get("/", function(req, res) {
