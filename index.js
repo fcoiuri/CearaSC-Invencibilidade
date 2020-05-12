674b759ad1f2edf47b5d2ddad89c155d40cfe9cc
FranciscoIuri <fcoiuri@gmail.com>
2020-05-12 19:05:17 -0300

    CSS

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const app = express()
 
-
 app.set("view engine", "ejs")
 app.set("views", "./views")
 
+app.use(express.static('./views'))
 app.listen(process.env.PORT || 3000);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("home.ejs")
 })
