f64df31ecd309053a505d2ac6774b465d3fa1926
FranciscoIuri <fcoiuri@gmail.com>
2020-05-20 14:11:39 -0300

    atualizando css e repositorios

--- resources/js/HoraDia.js
+++ resources/js/HoraDia.js
@@ -1,4 +1,24 @@
-var data = new Date()
-document.write(`Hora: ${data.getHours()}:${data.getMinutes()}:${data.getSeconds()}`)
-document.write("<br>")
-document.write(`Dia: ${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`)
+function tempo() {
+    var data = new Date();
+    var hora = data.getHours();
+    var minuto = data.getMinutes();
+    var segundo = data.getSeconds();
+    var dia = data.getDate()
+    var mes = data.getMonth() + 1
+    var ano = data.getFullYear()
+
+    minuto = checarTempo(minuto);
+    segundo = checarTempo(segundo);
+    mes = checarTempo(mes)
+    dia = checarTempo(dia)
+
+    document.getElementById('textoP').innerHTML =
+        "Hora: " + hora + ":" + minuto + ":" + segundo + "<br>" +
+        `Dia: ${dia}/${mes}/${ano}`;
+    var t = setTimeout(tempo, 500);
+}
+
+function checarTempo(i) {
+    if (i < 10) { i = "0" + i };
+    return i;
+}
